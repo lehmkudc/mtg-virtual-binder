63a4c315be1f09aeee31018be0e941758ae7d3a3
lehmkudc <lehmkudc@gmail.com>
2022-01-04 19:58:50 -0500

    first major push of sandbox work

---
+++ inventory_functions.R
@@ -0,0 +1,131 @@
+addCard <- function(cardName, Qty){
+  
+  cardName <- checkName(cardName)
+  
+  currentCollection <- fread("owned.csv")
+  
+  haveCard <- currentCollection %>%
+    filter( name == cardName ) %>%
+    nrow() > 0
+  
+  if( haveCard ){
+    currentCollection[name == cardName, qty := qty + Qty]
+  } else {
+    rbind( currentCollection, data.table(name = cardName, qty = Qty))
+  }
+  
+  fwrite( currentCollection, file = "owned.csv")
+  
+  return( 
+    currentCollection %>%
+      filter( name == cardName | 1:n() > n() - 5  )
+    
+  )
+}
+
+getCardPrice <- function( cardName, check_name = TRUE, return_all = FALSE, full_bling = FALSE ){
+  
+  if (check_name){
+    cardName <- checkName(cardName)
+  }
+  
+  if (cardName %in% c("Plains", "Island", "Swamp", "Mountain", "Forest")){
+    return( 0 )
+  }
+  cardName <- URLencode(cardName)
+  cardData <- glue("https://api.scryfall.com/cards/search?q={cardName}&unique=prints&include_variations=true&include_extras=true") %>%
+    fromJSON()
+  
+  if (return_all){
+    allData <- cardData$data %>%
+      as.data.table() %>%
+      filter( name == URLdecode(cardName)) %>%
+      select( object, name, games, set_name, digital, border_color, prices.usd, variation, set, collector_number, set_type) %>%
+      arrange( prices.usd)
+    
+    return( allData )
+  }
+  
+  if (full_bling){
+    blingest <- cardData$data %>%
+      as.data.table() %>%
+      select( name, games, prices.usd, prices.usd_foil, prices.usd_etched ) %>%
+      filter( name == URLdecode(cardName)) %>%
+      filter( grepl( pattern = "paper", x = games)) %>%
+      mutate_at( .vars = c("prices.usd", "prices.usd_foil", "prices.usd_etched"),.funs = as.numeric) %>%
+      mutate( blingest = pmax( prices.usd, prices.usd_foil, prices.usd_etched, na.rm = TRUE)) %>%
+      filter( blingest == max( blingest, na.rm = TRUE )) %>%
+      head(1) 
+    
+    return( blingest$blingest )
+  }
+  
+  cheapest <- cardData$data %>%
+    as.data.table() %>%
+    select( object, name, games, set_name, digital, border_color, prices.usd, variation, set, collector_number, set_type) %>%
+    filter( name == URLdecode(cardName)) %>%
+    filter( grepl( pattern = "paper", x = games)) %>%
+    mutate( prices.usd = as.numeric(prices.usd)) %>%
+    filter( prices.usd == min(prices.usd, na.rm = TRUE)) %>%
+    head( 1 )
+  
+  return( cheapest$prices.usd )
+}
+
+
+
+
+checkName <- function(cardName){
+  cardName <- URLencode(cardName)
+  
+  correctName <- glue("https://api.scryfall.com/cards/named?fuzzy={cardName}") %>%
+    fromJSON() %>%
+    .$name
+  
+  return( correctName )
+}
+
+
+
+
+parseDecklist <- function( filepath ){
+  
+  lines <- readLines(filepath)
+  break_pt <- which( lines == "")
+  
+  reg <- "^(\\w+)\\s?(.*)$"
+  main <- data.frame(raw = lines[1:(break_pt-1)]) %>%
+    mutate(
+      qty = sub( reg, "\\1", raw ),
+      cardname = sub( reg, "\\2", raw ),
+      board = "main"
+    )
+  
+  side <- data.frame(raw = lines[(break_pt+1):length(lines)]) %>%
+    mutate(
+      qty = sub( reg, "\\1", raw ),
+      cardname = sub( reg, "\\2", raw ),
+      board = "side"
+    )
+  
+  deck <- rbind( main, side ) %>%
+    select( -raw ) %>%
+    mutate(qty = as.integer(qty))
+  
+  return( deck )
+}
+
+
+getDeckPrices <- function( deck, full_bling = FALSE ){
+  prices <- lapply( unique(deck$cardname), function(cardname){
+    price_per_card <- getCardPrice(cardname, check_name = TRUE, full_bling = full_bling)
+    
+    return( data.table(cardname = cardname, price_per_card = price_per_card))
+  }) %>% bind_rows
+  
+  deckPrices <- deck %>%
+    left_join( prices, by = 'cardname' ) %>%
+    mutate( price_of_set = price_per_card * qty )
+  
+  return( deckPrices )
+}
---
+++ sandbox.R
@@ -0,0 +1,156 @@
+
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(data.table)
+  library(jsonlite)
+  library(glue)
+  library(RMariaDB)
+  library(pool)
+})
+
+mtg_pool <- dbPool(drv = MySQL(), host = "ec2-18-191-98-150.us-east-2.compute.amazonaws.com" )
+
+source( "inventory_functions.R")
+
+
+collection <- fread( "owned.csv" ) %>%
+  mutate( owned_qty = qty )
+
+
+burnPrices <- "decklists/modern/Deck - Burn.txt" %>%
+  parseDecklist() %>%
+  getDeckPrices() %>%
+  mutate( deck_qty = qty )
+
+
+
+
+addCard <- function(cardName, Qty){
+  
+  cardName <- checkName(cardName)
+  
+  currentCollection <- fread("owned.csv")
+  
+  haveCard <- currentCollection %>%
+    filter( name == cardName ) %>%
+    nrow() > 0
+  
+  
+  if( haveCard ){
+    currentCollection[name == cardName, qty := qty + Qty]
+  } else {
+    currentCollection <- rbind( currentCollection, data.table(name = cardName, qty = Qty))
+  }
+  
+  fwrite( currentCollection, file = "owned.csv")
+  
+  return( 
+    currentCollection %>%
+      filter( name == cardName | 1:n() > n() - 5  )
+    
+  )
+}
+
+addCard(
+  "Ephemerate", 
+  4
+)
+
+
+
+getMyDeckPrice <- function(deck, return_total = FALSE){
+
+  if( typeof(deck) == "character"){
+    deck <- parseDecklist(deck)
+  }
+  
+  if( !('price_per_card' %in% colnames(deck)) ){
+    deck <- getDeckPrices(deck)
+  }
+  
+  deck <- deck %>%
+    mutate( deck_qty = qty )
+  
+  collection <- fread( "owned.csv" ) %>%
+    mutate( owned_qty = qty )
+  
+  price_diff <- deck %>%
+    left_join( collection, by = c("cardname" = "name") ) %>%
+    select( -qty.x, -qty.y, -price_of_set ) %>%
+    mutate( owned_qty = ifelse( is.na(owned_qty), 0, owned_qty)) %>%
+    mutate( needed_qty = deck_qty - owned_qty ) %>%
+    mutate( needed_qty = ifelse( needed_qty < 0, 0, needed_qty) ) %>%
+    filter( needed_qty > 0 ) %>%
+    mutate( price_of_remaining_set = needed_qty * price_per_card ) %>%
+    arrange( - price_per_card )
+  
+  if (return_total){
+    return(
+      price_diff %>%
+        group_by(board) %>%
+        summarise(
+          total_price = sum( price_of_remaining_set )
+        )
+    )
+  } else {
+    return( 
+      price_diff
+    )
+  }
+    
+}
+
+
+getDeckPrices <- function( deck ){
+  #browser()
+  prices <- lapply( deck$cardname, function(cardname){
+    price_per_card <- getCardPrice(cardname, check_name = FALSE)
+    
+    return( data.table(cardname = cardname, price_per_card = price_per_card))
+  }) %>% bind_rows
+  
+  deckPrices <- deck %>%
+    left_join( prices, by = 'cardname' ) %>%
+    mutate( price_of_set = price_per_card * qty )
+  
+  return( deckPrices )
+}
+
+filepath <- "decklists/legacy/Deck - Death and Taxes.txt"
+filepath <- "decklists/pauper/Deck - U.txt"
+filepath <- "decklists/legacy/Deck - Lands.txt"
+deck <- filepath %>% parseDecklist()
+deckPrices <- deck %>% getDeckPrices(full_bling = TRUE)
+
+deckPrices %>%
+  group_by( board ) %>%
+  arrange( -price_of_set )
+
+deckPrices %>%
+  group_by( board ) %>%
+  arrange( -price_of_set ) %>%
+  summarize( total_price = sum( price_of_set ))
+
+getMyDeckPrice( deckPrices, return_total = TRUE )
+getMyDeckPrice( deckPrices, return_total = FALSE)
+
+
+parseDecklist("decklists/legacy/Deck - Death and Taxes.txt")
+
+cardData$data %>%
+  as.data.table() %>%
+  select( name, games, prices.usd, prices.usd_foil, prices.usd_etched)
+
+
+
+
+
+con <- dbConnect(
+  drv = MariaDB(),
+  host = "localhost",
+  port = 3306,
+  username = "marina",
+  password = "cool_beans",
+  dbname = "marina_inventory"
+)
+
